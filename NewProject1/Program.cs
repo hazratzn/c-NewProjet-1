5b9ced5eb67de04d2bca89ed5f62342e4a1d0aba
unknown <hazratzn@code.edu.az>
2021-11-26 22:53:10 -0800

    New Project

---
+++ NewProject1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NewProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 25;
+
+            num1 = num1 + num2;
+
+            num2 = num1 - num2;
+
+            num1 = num1 - num2;
+
+
+            Console.WriteLine(num1 + " " + num2);
+        }
+    }
+}
